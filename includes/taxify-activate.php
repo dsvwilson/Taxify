3f59cb040422d508c05c054ad68a648eb32bbc1f
Victor Wilson <63019070+dsvwilson@users.noreply.github.com>
2023-02-03 05:43:00 +0100

    Housekeeping

--- includes/taxify-activate.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Activate {
-    public static function activate() {
-        $this->custom_post_type(); // calling the Taxes custom post type here in case it failed initially    
-        flush_rewrite_rules(); // true by default
-    }
-}
--- includes/taxify-deactivate.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Deactivate {
-    public static function deactivate() { 
-        flush_rewrite_rules(); // true by default
-    }
-}
